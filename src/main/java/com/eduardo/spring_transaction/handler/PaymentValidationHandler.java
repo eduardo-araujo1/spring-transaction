b9ca2b2acc15dedf65527873b832b3955359284d
Eduardo Araujo <eduardo1996araujo@gmail.com>
2025-01-15 20:57:04 -0300

    feat - adicionado readme do projeto

--- src/main/java/com/eduardo/spring_transaction/handler/PaymentValidationHandler.java
+++ src/main/java/com/eduardo/spring_transaction/handler/PaymentValidationHandler.java
@@ -6,6 +6,8 @@ import com.eduardo.spring_transaction.repository.AuditLogRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +16,7 @@ public class PaymentValidationHandler {
 
     private static final Logger log = LoggerFactory.getLogger(PaymentValidationHandler.class);
 
+    //@Transactional(propagation = Propagation.NESTED) O banco H2 não suporta o NESTED
     public void validatePayment(Order order) {
         boolean successful = isPaymentSuccessful(order);
 
