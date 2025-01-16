ccd6ca5b91adb888e644b6fd124d74b460091dff
Eduardo Araujo <eduardo1996araujo@gmail.com>
2025-01-15 21:24:48 -0300

    refactor - retirando o nested

--- src/main/java/com/eduardo/spring_transaction/handler/PaymentValidationHandler.java
+++ src/main/java/com/eduardo/spring_transaction/handler/PaymentValidationHandler.java
@@ -1,22 +1,15 @@
 package com.eduardo.spring_transaction.handler;
 
-import com.eduardo.spring_transaction.entity.AuditLog;
 import com.eduardo.spring_transaction.entity.Order;
-import com.eduardo.spring_transaction.repository.AuditLogRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 public class PaymentValidationHandler {
 
     private static final Logger log = LoggerFactory.getLogger(PaymentValidationHandler.class);
 
-    //@Transactional(propagation = Propagation.NESTED) O banco H2 não suporta o NESTED
     public void validatePayment(Order order) {
         boolean successful = isPaymentSuccessful(order);
 
